2b7ee0a262fab8c0a5bb5fc495a6424f4cd5090b
Jesper Rosenlund Nielsen <jesper@rosenlundnielsen.net>
2026-04-19 13:52:50 +0200

    Update dependencies and refactor IP determination logic to use network-default-gateway

--- test/test-live-outgoing-player-state.js
+++ test/test-live-outgoing-player-state.js
@@ -12,26 +12,45 @@ try {
     console.log(e)
 }
 
-const ip = require('internal-ip').v4.sync();
-
-
-
-if (RouvyPacketMonitor && Cap) {
-
-    console.log('Listening on: ', ip, JSON.stringify(Cap.findDevice(ip),null,4));
-    
-    // determine network interface associated with external IP address
-    interface = Cap.findDevice(ip);
-    // ... and setup monitor on that interface:
-    const monitor = new RouvyPacketMonitor(interface)
-    
-
-    
-    monitor.on('outgoingPlayerState', (playerState) => {
-        console.log(playerState)
-    })
-    
-    
-    monitor.start()
+const { v4DefaultGateway } = require('network-default-gateway');
+// Determine bind IP via network-default-gateway
+let ip;
+try {
+    const defaultGateway = v4DefaultGateway();
+} catch (e) {
+    /** @type {any} */
+    const err = e;
+    console.log('Could not determine default gateway IP:', err?.message ?? err);
 }
 
+// wait for the default gateway promise to resolve
+v4DefaultGateway().then((defaultGateway) => {
+    ip = defaultGateway?.ip;
+    if (!ip) {
+        console.log('Could not determine default gateway IP');
+    } else {
+        console.log('Default gateway IP:', ip);
+    }
+
+    if (ip && RouvyPacketMonitor && Cap) {
+
+        
+        // determine network interface associated with external IP address
+        interface = Cap.findDevice(ip);
+        console.log('Listening on: ', ip, JSON.stringify(interface, null, 4));
+        // ... and setup monitor on that interface:
+        const monitor = new RouvyPacketMonitor(interface)
+        
+
+        
+        monitor.on('outgoingPlayerState', (playerState) => {
+            console.log(playerState)
+        })
+        
+        
+        monitor.start()
+    }
+
+}).catch((err) => {
+    console.log('Error determining default gateway IP:', err);
+})
